b3e99d8305f0927c86e2503a3f74b753fa26bddd
phxswjjj <phxswjjj@gmail.com>
2024-09-10 23:18:43 +0800

    增加集結偵錯訊息

--- game/worker/build_up.py
+++ game/worker/build_up.py
@@ -13,7 +13,7 @@ class BuildUpWorker:
     def set_next_time_to_fight(self):
         self.next_time_to_fight = datetime.now() + timedelta(seconds=5)
 
-    def is_time_to_collect(self) -> bool:
+    def is_time_to_fight(self) -> bool:
         return datetime.now() > self.next_time_to_fight
 
     def is_in_build_up(self) -> bool:
@@ -25,32 +25,38 @@ class BuildUpWorker:
 
     def build_up(self) -> bool:
 
-        if not self.is_time_to_collect():
+        if not self.is_time_to_fight():
             return False
 
         if not self.is_in_build_up():
             return False
         
         try:
+            # 有空的位置
             empty_slot_pos = pyautogui.locateOnScreen(self.game_info.build_up_empty_slot_img_path, confidence=0.8)
             button_center = pyautogui.center(empty_slot_pos)
             pyautogui.click(button_center)
-            time.sleep(0.1)
         except pyautogui.ImageNotFoundException:
             return False
         
+        time.sleep(0.2)
+        
         try:
+            # 套用第8軍團
             pos = pyautogui.locateOnScreen(self.game_info.build_up_army8_img_path, confidence=0.8)
             button_center = pyautogui.center(pos)
             pyautogui.click(button_center)
-            time.sleep(0.1)
-            # fight button
-            pyautogui.click((959, 491))
-            
-            self.set_next_time_to_fight()
-            return True
         except pyautogui.ImageNotFoundException:
+            print("找不到第8軍團圖片")
             return False
+        
+        time.sleep(0.2)
+
+        # fight button
+        pyautogui.click((959, 491))
+        
+        self.set_next_time_to_fight()
+        return True
         
         
         
