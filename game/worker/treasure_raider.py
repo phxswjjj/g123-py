f649b0df9b860c005136337fe6ea29cd19ecc157
phxswjjj <phxswjjj@gmail.com>
2024-10-27 21:14:07 +0800

    增加鍵盤移動

--- game/worker/treasure_raider.py
+++ game/worker/treasure_raider.py
@@ -4,6 +4,7 @@ import logging
 import time
 
 import inject
+import keyboard
 import pyautogui
 from game.game_info import GameInfo
 
@@ -19,7 +20,7 @@ class TreasureRaiderWorker:
             self.set_next_time_to_fire()
 
     def set_next_time_to_fire(self):
-        self.next_time_to_fire = datetime.now() + timedelta(seconds=10)
+        self.next_time_to_fire = datetime.now() + timedelta(minutes=1)
 
     def is_time_to_fire(self) -> bool:
         return datetime.now() > self.next_time_to_fire
@@ -33,13 +34,56 @@ class TreasureRaiderWorker:
         
     def search_and_click_raider(self) -> bool:
         try:
-            pos = pyautogui.locateOnScreen(self.game_info.map_treasure_raider_img_path, confidence=0.9)
+            pos = pyautogui.locateOnScreen(self.game_info.map_treasure_raider_img_path, confidence=0.8)
             button_center = pyautogui.center(pos)
             pyautogui.click(button_center)
             time.sleep(1)
             return True
         except pyautogui.ImageNotFoundException:
             return False
+        
+    def move_next_screen(self) -> bool:
+        cneter_pos = (979, 525)
+        move_duration = 0.5
+        move_distanceX = 1400
+        move_distanceY = 500
+        if keyboard.is_pressed('right'):
+            pyautogui.moveTo(cneter_pos[0] + move_distanceX/2, cneter_pos[1])
+            time.sleep(0.1)
+            
+            pyautogui.mouseDown()
+            pyautogui.dragTo(cneter_pos[0] - move_distanceX/2, cneter_pos[1], duration=move_duration)
+            pyautogui.mouseUp()
+            time.sleep(0.1)
+            return True
+        elif keyboard.is_pressed('left'):
+            pyautogui.moveTo(cneter_pos[0] - move_distanceX/2, cneter_pos[1])
+            time.sleep(0.1)
+            
+            pyautogui.mouseDown()
+            pyautogui.dragTo(cneter_pos[0] + move_distanceX/2, cneter_pos[1], duration=move_duration)
+            pyautogui.mouseUp()
+            time.sleep(0.1)
+            return True
+        elif keyboard.is_pressed('up'):
+            pyautogui.moveTo(cneter_pos[0], cneter_pos[1] - move_distanceY/2)
+            time.sleep(0.1)
+            
+            pyautogui.mouseDown()
+            pyautogui.dragTo(cneter_pos[0], cneter_pos[1] + move_distanceY/2, duration=move_duration)
+            pyautogui.mouseUp()
+            time.sleep(0.1)
+            return True
+        elif keyboard.is_pressed('down'):
+            pyautogui.moveTo(cneter_pos[0], cneter_pos[1] + move_distanceY/2)
+            time.sleep(0.1)
+            
+            pyautogui.mouseDown()
+            pyautogui.dragTo(cneter_pos[0], cneter_pos[1] - move_distanceY/2, duration=move_duration)
+            pyautogui.mouseUp()
+            time.sleep(0.1)
+            return True
+        return False
 
     def search_raider(self) -> bool:
         if not self.is_time_to_fire():
@@ -52,6 +96,7 @@ class TreasureRaiderWorker:
             return False
         
         if not self.search_and_click_raider():
+            self.move_next_screen()
             return False
 
         self.logger.info("找到敵方奪寶基地")
